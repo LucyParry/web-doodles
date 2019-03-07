6337b37e9a2072838cfef25b567053bf978c6e5c
Lucy <lucy.parry@gmail.com>
2019-03-07 23:25:40 +0000

    Add wisdom dispenser on bottom jumbotron!

---
+++ deadlines/scripts/wisdom.js
@@ -0,0 +1,67 @@
+function getRandomWisdom() {
+    var randomInt = getRandomInt(wisdom.length);
+    return wisdom[randomInt];
+}
+
+var wisdom = [
+    {
+        "text": "Hofstadter's Law: It always takes longer than you expect, even when you take into account Hofstadter's Law.",
+        "author": "Douglas Hofstadter",
+        "submittedBy": ""
+    },
+    {
+        "text": "If it's a good idea, go ahead and do it. It is much easier to apologise than it is to get permission.",
+        "author": "Rear Admiral Grace Hopper",
+        "submittedBy": ""
+    },
+    {
+        "text": "Those who plan do better than those who do not plan, even though they rarely stick to their plan.",
+        "author": "Winston Churchill",
+        "submittedBy": ""
+    },
+    {
+        "text": "We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins.",
+        "author": "Ellen Ullman",
+        "submittedBy": ""
+    },
+    {
+        "text": "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+        "author": "Martin Fowler",
+        "submittedBy": ""
+    },
+    {
+        "text": "All information belongs to everybody all the time. It should be available. It should be accessible to the child, to the woman, to the man, to the old person, to the semiliterate, to the presidents of universities, to everyone. It should be open.",
+        "author": "Maya Angelou",
+        "submittedBy": ""
+    },
+    {
+        "text": "If I had more time, I would have written a shorter letter.",
+        "author": "Cicero",
+        "submittedBy": ""
+    },
+    {
+        "text": "Know how to learn. Then, want to learn.",
+        "author": "Katherine Johnson",
+        "submittedBy": ""
+    },
+    {
+        "text": "I love deadlines. I like the whooshing sound they make as they fly by.",
+        "author": "Douglas Adams",
+        "submittedBy": ""
+    },
+    {
+        "text": "The best thing about a Boolean is even if you are wrong, you are only off by a bit.",
+        "author": "Anonymous",
+        "submittedBy": ""
+    },
+    {
+        "text": "Weeks of coding can save you hours of planning.",
+        "author": "Anonymous",
+        "submittedBy": ""
+    },
+    {
+        "text": "Get your facts first, then you can distort them as you please",
+        "author": "Mark Twain",
+        "submittedBy": ""
+    }
+];
