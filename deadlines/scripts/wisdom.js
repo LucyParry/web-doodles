fd656f6da3aa4ea5f8baf1dea5bd58fca8568972
Lucy <lucy.parry@gmail.com>
2019-04-04 13:03:39 +0100

    Hmmmm, revert back

--- deadlines/scripts/wisdom.js
+++ deadlines/scripts/wisdom.js
@@ -6,60 +6,51 @@ function getRandomWisdom() {
 var wisdom = [
     {
         "text": "Hofstadter's Law: It always takes longer than you expect, even when you take into account Hofstadter's Law.",
-        "author": "<a href='https://en.wikipedia.org/wiki/Douglas_Hofstadter'>Douglas Hofstadter</a>",
+        "author": "Douglas Hofstadter",
         "submittedBy": ""
     },
     {
         "text": "If it's a good idea, go ahead and do it. It is much easier to apologise than it is to get permission.",
-        "author": "<a href='https://en.wikipedia.org/wiki/Grace_Hopper'>Rear Admiral Grace Hopper</a>",
+        "author": "Rear Admiral Grace Hopper",
         "submittedBy": ""
     },
     {
         "text": "Those who plan do better than those who do not plan, even though they rarely stick to their plan.",
-        "author": "<a href='https://en.wikipedia.org/wiki/Winston_Churchill'>Winston Churchill</a>",
+        "author": "Winston Churchill",
         "submittedBy": ""
     },
     {
         "text": "We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins.",
-        "author": "<a href='https://en.wikipedia.org/wiki/Ellen_Ullman'>Ellen Ullman</a>",
+        "author": "Ellen Ullman",
         "submittedBy": ""
     },
     {
-        "text": "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",'        "author": "<a href='https://en.wikipedia.org/wiki/Martin_Fowler_(software_engineer)'>Martin Fowler</a>",
+        "text": "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+        "author": "Martin Fowler",
         "submittedBy": ""
     },
     {
         "text": "All information belongs to everybody all the time. It should be available. It should be accessible to the child, to the woman, to the man, to the old person, to the semiliterate, to the presidents of universities, to everyone. It should be open.",
-        "author": "<a href='https://en.wikipedia.org/wiki/Maya_Angelou'>Maya Angelou</a>",
+        "author": "Maya Angelou",
         "submittedBy": ""
     },
     {
         "text": "If I had more time, I would have written a shorter letter.",
-        "author": "<a href='https://en.wikipedia.org/wiki/Cicero'>Cicero</a>",
+        "author": "Cicero",
         "submittedBy": ""
     },
     {
         "text": "Know how to learn. Then, want to learn.",
-        "author": "<a href='https://en.wikipedia.org/wiki/Katherine_Johnson'>Katherine Johnson</a>",
+        "author": "Katherine Johnson",
         "submittedBy": ""
     },
     {
         "text": "I love deadlines. I like the whooshing sound they make as they fly by.",
-        "author": "<a href='https://en.wikipedia.org/wiki/Douglas_Adams'>Douglas Adams</a>",
+        "author": "Douglas Adams",
         "submittedBy": ""
     },
     {
-        "text": "(on the Open University) ... a great independent university which does not insult any man or any woman whatever their background by offering them the second best, nothing but the best is good enough.",
-        "author": "<a href='https://en.wikipedia.org/wiki/Jennie_Lee,_Baroness_Lee_of_Asheridge'>Jennie Lee, Baroness Lee of Asheridge</a>",
-        "submittedBy": ""
-    },
-    {
-        "text": "Get your facts first, then you can distort them as you please",
-        "author": "<a href='https://en.wikipedia.org/wiki/Mark_Twain'>Mark Twain</a>",
-        "submittedBy": ""
-    },
-    {
-        "text": "The best thing about a Boolean is, even if you are wrong, you are only off by a bit.",
+        "text": "The best thing about a Boolean is even if you are wrong, you are only off by a bit.",
         "author": "Anonymous",
         "submittedBy": ""
     },
@@ -69,8 +60,8 @@ var wisdom = [
         "submittedBy": ""
     },
     {
-        "text": "Roses are red, Violets are blue, Unexpected { on line 32",
-        "author": "Anonymous",
+        "text": "Get your facts first, then you can distort them as you please",
+        "author": "Mark Twain",
         "submittedBy": ""
     }
 ];
