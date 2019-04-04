46643e2ce715b3a4c7b4a9cfc0da7982c701a19a
Lucy <lucy.parry@gmail.com>
2019-04-04 12:52:56 +0100

    Oops... should have used single quotes

--- deadlines/scripts/wisdom.js
+++ deadlines/scripts/wisdom.js
@@ -6,57 +6,57 @@ function getRandomWisdom() {
 var wisdom = [
     {
         "text": "Hofstadter's Law: It always takes longer than you expect, even when you take into account Hofstadter's Law.",
-        "author": "<a href="https://en.wikipedia.org/wiki/Douglas_Hofstadter">Douglas Hofstadter</a>",
+        "author": "<a href='https://en.wikipedia.org/wiki/Douglas_Hofstadter'>Douglas Hofstadter</a>",
         "submittedBy": ""
     },
     {
         "text": "If it's a good idea, go ahead and do it. It is much easier to apologise than it is to get permission.",
-        "author": "<a href="https://en.wikipedia.org/wiki/Grace_Hopper">Rear Admiral Grace Hopper</a>",
+        "author": "<a href='https://en.wikipedia.org/wiki/Grace_Hopper'>Rear Admiral Grace Hopper</a>",
         "submittedBy": ""
     },
     {
         "text": "Those who plan do better than those who do not plan, even though they rarely stick to their plan.",
-        "author": "<a href="https://en.wikipedia.org/wiki/Winston_Churchill">Winston Churchill</a>",
+        "author": "<a href='https://en.wikipedia.org/wiki/Winston_Churchill'>Winston Churchill</a>",
         "submittedBy": ""
     },
     {
         "text": "We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins.",
-        "author": "<a href="https://en.wikipedia.org/wiki/Ellen_Ullman">Ellen Ullman</a>",
+        "author": "<a href='https://en.wikipedia.org/wiki/Ellen_Ullman'>Ellen Ullman</a>",
         "submittedBy": ""
     },
     {
         "text": "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-        "author": "<a href="https://en.wikipedia.org/wiki/Martin_Fowler_(software_engineer)">Martin Fowler</a>",
+        "author": "<a href='https://en.wikipedia.org/wiki/Martin_Fowler_(software_engineer)'>Martin Fowler</a>",
         "submittedBy": ""
     },
     {
         "text": "All information belongs to everybody all the time. It should be available. It should be accessible to the child, to the woman, to the man, to the old person, to the semiliterate, to the presidents of universities, to everyone. It should be open.",
-        "author": "<a href="https://en.wikipedia.org/wiki/Maya_Angelou">Maya Angelou</a>",
+        "author": "<a href='https://en.wikipedia.org/wiki/Maya_Angelou'>Maya Angelou</a>",
         "submittedBy": ""
     },
     {
         "text": "If I had more time, I would have written a shorter letter.",
-        "author": "<a href="https://en.wikipedia.org/wiki/Cicero">Cicero</a>",
+        "author": "<a href='https://en.wikipedia.org/wiki/Cicero'>Cicero</a>",
         "submittedBy": ""
     },
     {
         "text": "Know how to learn. Then, want to learn.",
-        "author": "<a href="https://en.wikipedia.org/wiki/Katherine_Johnson">Katherine Johnson</a>",
+        "author": "<a href='https://en.wikipedia.org/wiki/Katherine_Johnson'>Katherine Johnson</a>",
         "submittedBy": ""
     },
     {
         "text": "I love deadlines. I like the whooshing sound they make as they fly by.",
-        "author": "<a href="https://en.wikipedia.org/wiki/Douglas_Adams">Douglas Adams</a>",
+        "author": "<a href='https://en.wikipedia.org/wiki/Douglas_Adams'>Douglas Adams</a>",
         "submittedBy": ""
     },
     {
         "text": "(on the Open University) ... a great independent university which does not insult any man or any woman whatever their background by offering them the second best, nothing but the best is good enough.",
-        "author": "<a href="https://en.wikipedia.org/wiki/Jennie_Lee,_Baroness_Lee_of_Asheridge">Jennie Lee, Baroness Lee of Asheridge</a>",
+        "author": '<a href="https://en.wikipedia.org/wiki/Jennie_Lee,_Baroness_Lee_of_Asheridge'>Jennie Lee, Baroness Lee of Asheridge</a>",
         "submittedBy": ""
     },
     {
         "text": "Get your facts first, then you can distort them as you please",
-        "author": "<a href="https://en.wikipedia.org/wiki/Mark_Twain">Mark Twain</a>",
+        "author": "<a href='https://en.wikipedia.org/wiki/Mark_Twain'>Mark Twain</a>",
         "submittedBy": ""
     },
     {
