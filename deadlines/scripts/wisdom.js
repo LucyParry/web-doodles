2a9f67484497db905b62e594f3e66e4bce20431e
Lucy <lucy.parry@gmail.com>
2019-04-04 21:22:43 +0100

    Add more wisdom

--- deadlines/scripts/wisdom.js
+++ deadlines/scripts/wisdom.js
@@ -11,46 +11,76 @@ var wisdom = [
     },
     {
         "text": "If it's a good idea, go ahead and do it. It is much easier to apologise than it is to get permission.",
-        "author": "Rear Admiral Grace Hopper",
+        "author": "<a href='https://en.wikipedia.org/wiki/Grace_Hopper'>Rear Admiral Grace Hopper</a>",
         "submittedBy": ""
     },
     {
         "text": "Those who plan do better than those who do not plan, even though they rarely stick to their plan.",
-        "author": "Winston Churchill",
+        "author": "<a href='https://en.wikipedia.org/wiki/Winston_Churchill'>Winston Churchill</a>",
         "submittedBy": ""
     },
     {
         "text": "We build our computer (systems) the way we build our cities: over time, without a plan, on top of ruins.",
-        "author": "Ellen Ullman",
+        "author": "<a href='https://en.wikipedia.org/wiki/Ellen_Ullman'>Ellen Ullman</a>",
         "submittedBy": ""
     },
     {
         "text": "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-        "author": "Martin Fowler",
+        "author": "<a href='https://en.wikipedia.org/wiki/Martin_Fowler_(software_engineer)'>Martin Fowler</a>",
         "submittedBy": ""
     },
     {
         "text": "All information belongs to everybody all the time. It should be available. It should be accessible to the child, to the woman, to the man, to the old person, to the semiliterate, to the presidents of universities, to everyone. It should be open.",
-        "author": "Maya Angelou",
+        "author": "<a href='https://en.wikipedia.org/wiki/Maya_Angelou'>Maya Angelou</a>",
         "submittedBy": ""
     },
     {
         "text": "If I had more time, I would have written a shorter letter.",
-        "author": "Cicero",
+        "author": "<a href='https://en.wikipedia.org/wiki/Cicero'>Cicero</a>",
         "submittedBy": ""
     },
     {
         "text": "Know how to learn. Then, want to learn.",
-        "author": "Katherine Johnson",
+        "author": "<a href='https://en.wikipedia.org/wiki/Katherine_Johnson'>Katherine Johnson</a>",
         "submittedBy": ""
     },
     {
         "text": "I love deadlines. I like the whooshing sound they make as they fly by.",
-        "author": "Douglas Adams",
+        "author": "<a href='https://en.wikipedia.org/wiki/Douglas_Adams'>Douglas Adams</a>",
         "submittedBy": ""
     },
     {
-        "text": "The best thing about a Boolean is even if you are wrong, you are only off by a bit.",
+        "text": "(on the Open University) ... a great independent university which does not insult any man or any woman whatever their background by offering them the second best, nothing but the best is good enough.",
+        "author": "<a href='https://en.wikipedia.org/wiki/Jennie_Lee,_Baroness_Lee_of_Asheridge'> Jennie Lee, Baroness Lee of Asheridge</a>",
+        "submittedBy": ""
+    },
+    {
+        "text": "Get your facts first, then you can distort them as you please",
+        "author": "<a href='https://en.wikipedia.org/wiki/Mark_Twain'>Mark Twain</a>",
+        "submittedBy": ""
+    },
+    {
+        "text": "Science is knowledge which we understand so well that we can teach it to a computer; and if we don't fully understand something, it is an art to deal with it.",
+        "author": "<a href='https://en.wikipedia.org/wiki/Donald_Knuth'>Donald Knuth</a>",
+        "submittedBy": ""
+    },
+    {
+        "text": "To me programming is more than an important practical art. It is also a gigantic undertaking in the foundations of knowledge.",
+        "author": "<a href='https://en.wikipedia.org/wiki/Grace_Hopper'>Rear Admiral Grace Hopper</a>",
+        "submittedBy": ""
+    },
+    {
+        "text": "I have a hunch that the unknown sequences of DNA will decode into copyright notices and patent protections.",
+        "author": "<a href='https://en.wikipedia.org/wiki/Donald_Knuth'>Donald Knuth</a>",
+        "submittedBy": ""
+    },
+    {
+        "text": "Understand well as I may, my comprehension can only be an infinitesimal fraction of all I want to understand.",
+        "author": "<a href='https://en.wikipedia.org/wiki/Ada_Lovelace'>Ada Lovelace</a>",
+        "submittedBy": ""
+    },
+    {
+        "text": "The best thing about a Boolean is, even if you are wrong, you are only off by a bit.",
         "author": "Anonymous",
         "submittedBy": ""
     },
@@ -60,8 +90,8 @@ var wisdom = [
         "submittedBy": ""
     },
     {
-        "text": "Get your facts first, then you can distort them as you please",
-        "author": "Mark Twain",
+        "text": "Roses are red, Violets are blue, Unexpected { on line 32",
+        "author": "Anonymous",
         "submittedBy": ""
     }
 ];
